094411e998e49f281a0f912477ab98d8a74a4e8d
Philip Rinn <rinni@inventati.org>
2016-07-06 22:55:56 +0200

    Release version 0.5.2

--- gtranscribe/AboutDialog.py
+++ gtranscribe/AboutDialog.py
@@ -30,7 +30,7 @@ class AboutDialog:
         self.about_dialog.set_transient_for(trans.window)
         self.about_dialog.set_destroy_with_parent(True)
         self.about_dialog.set_name("gTranscribe")
-        self.about_dialog.set_version("0.5.1")
+        self.about_dialog.set_version("0.5.2")
         self.about_dialog.set_copyright("Copyright \xc2\xa9 2013-2016 Philip Rinn\nCopyright \xc2\xa9 2010 Frederik Elwert")
         self.about_dialog.set_comments(_("gTranscribe is a software focussed on easy transcription of spoken words."))
         self.about_dialog.set_website("https://github.com/innir/gtranscribe")
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ assert DistUtilsExtra.auto.__version__ >= '2.22', 'gTranscribe needs DistUtilsEx
 
 DistUtilsExtra.auto.setup(
     name='gTranscribe',
-    version='0.5.1',
+    version='0.5.2',
     license='GPL-3',
     author='Philip Rinn',
     author_email='rinni@inventati.org',
