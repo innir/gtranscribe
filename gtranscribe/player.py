58e06cfdf1b11ce85d819b8e77f36e4341f2cee4
Philip Rinn <rinni@inventati.org>
2020-12-06 19:05:36 +0100

    Make pylint a little happier

--- gtranscribe/player.py
+++ gtranscribe/player.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-#pylint: disable=wrong-import-position
+# pylint: disable=wrong-import-position
 import logging
 import gi
 gi.require_version('Gst', '1.0')
